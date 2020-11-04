2efd11d22f25a6237de0cffd460b97c76080e07b
Maná Machado <manamachado8@gmail.com>
2020-11-04 15:33:24 -0300

    2.2. comportamento de classes

--- js-programando-a-orientacao-a-objetos/index.js
+++ js-programando-a-orientacao-a-objetos/index.js
@@ -1,22 +1,36 @@
 class Client {
   name;
   cpf;
+}
+
+class CurrentAccount {
   agency;
   balance;
+
+  withdraw(value) {
+    if (this.balance >= value) {
+      this.balance -= value;
+    }
+  }
 }
 
 const client1 = new Client();
-const client2 = new Client();
-
 client1.name = 'Fulano';
 client1.cpf = 11222333;
-client1.agency = 1001;
-client1.balance = 0;
 
+const client2 = new Client();
 client2.name = 'Ciclano';
 client2.cpf = 11222444;
-client2.agency = 1001;
-client2.balance = 0;
 
-console.log(client1);
-console.log(client2);
+const client1CurrentAccount = new CurrentAccount();
+client1CurrentAccount.balance = 0;
+client1CurrentAccount.agency = 1001;
+
+// console.log(client1);
+// console.log(client2);
+
+console.log(client1CurrentAccount.balance);
+client1CurrentAccount.balance = 100;
+console.log(client1CurrentAccount.balance);
+client1CurrentAccount.withdraw(50);
+console.log(client1CurrentAccount.balance);
